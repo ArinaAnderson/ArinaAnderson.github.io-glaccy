e2cca7171b793d06a039e343d7bda9838772a738
Marina <iolsoOfuolso@gmail.com>
2019-07-02 22:08:32 -0700

    adds dropdown-menu only for one item, needs to be reused for more items

--- src/js/scripts/main-nav-drop-down.js
+++ src/js/scripts/main-nav-drop-down.js
@@ -1,49 +1,5 @@
 'use strict';
 (function () {
-  var mainNavBtn = document.querySelector('.page-header__btn');
-  mainNavBtn.classList.remove('page-header__btn--no-js');
-
-
-  var breakPoints = [0, 600];
-  var devWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;//calculation + addEventL... window.addEventListener('resize', function() {calculation});
-  var devWidthBreakpoint = defineDevWidthBreakpoint();//defineDevWidthBreakpoint();//<-- defineDevWidthBreakpoint()
-  function defineDevWidthBreakpoint() {
-    return devWidth < 600 ? 0 : 600;
-  }
-  function windowResizeHandler() {
-    devWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
-    devWidthBreakpoint = defineDevWidthBreakpoint();
-  }
-
-  var navLists = [];
-
-  function NavList(listClass) {
-    this.list = document.querySelector('.' + listClass);
-    this.listCloseClass = listClass + '--closed';
-    this.openCloseNavList = function () {
-      this.list.classList.toggle(this.listCloseClass);
-    };
-  }
-
-  navLists.push(new NavList('site-list'));
-  navLists.push(new NavList('user-list'));
-  
-  window.addEventListener('resize', function () {
-    windowResizeHandler();
-  });
-
-  mainNavBtn.addEventListener('click', function () {
-    for (var i = 0; i < navLists.length; i++) {
-      navLists[i].openCloseNavList();
-    }
-    mainNavBtn.classList.toggle('page-header__btn--open');
-    mainNavBtn.classList.toggle('page-header__btn--close');
-    console.log(devWidthBreakpoint);
-  });
-})();
-
-'use strict';
-(function () {
   //OOP:
   /*var dropdownMenu = {
     dropdownList: document.querySelector('.site-list__dropdown '),
